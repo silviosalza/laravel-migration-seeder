bf912654ccd4d224fc1aecd75e155e5a4deada40
silviosalza <silviofilipposalza@gmail.com>
2023-05-25 15:51:44 +0200

    db create, migration tab added, model added

---
+++ database/migrations/2023_05_25_130711_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('company', 20);
+            $table->string('departure_station', 40);
+            $table->string('arrival_station', 40);
+            $table->string('code', 10);
+            $table->tinyInteger('total_couch')->nullable();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('canceled')->nullable();
+            $table->date('departure_date');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 30; $i++) {
+            $train = new Train();
+            $train->reference = $faker->bothify('??-#########');
+
+
+        }    
+    }
+}

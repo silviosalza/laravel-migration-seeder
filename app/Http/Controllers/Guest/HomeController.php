15e431239c3cc5eb98c1683651b61bf134f00a90
silviosalza <silviofilipposalza@gmail.com>
2023-05-25 17:07:20 +0200

    trains db captured

---
+++ app/Http/Controllers/Guest/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $trains = Train::all();
+        dd($trains);
+        return view('home', compact('trains'));
+    }
+}
